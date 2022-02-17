d787002f0078f12d7507a867e99f92b15c2b6ae1
Luiz Fernando Ribeiro dos Santos <82455649+feribeirods@users.noreply.github.com>
2022-02-17 00:12:48 -0300

    Add files via upload

---
+++ src/ProductStructure.php
@@ -0,0 +1,54 @@
+<?php
+namespace App;
+
+class ProductStructure
+{
+    const products = [
+        "preto-PP",
+        "preto-M",
+        "preto-G",
+        "preto-GG",
+        "preto-GG",
+        "branco-PP",
+        "branco-G",
+        "vermelho-M",
+        "azul-XG",
+        "azul-XG",
+        "azul-XG",
+        "azul-P"
+    ];
+
+    public function make(): array
+    {
+        $resultado = array();
+
+        $camisas = products;
+        
+        foreach($camisas as $camisa) {
+        
+            // Separa os valores da string em array
+            $produto = explode('-', $camisa);
+        
+            // Seleciona o tamanho e a cor do produto
+            $tamanho = $produto[1];
+            $cor = $produto[0];
+        
+            
+            // Atribui os valores e as quantidades
+            if(!isset($resultado[$cor][$tamanho])) {
+                $resultado[$cor][$tamanho] = 1;
+                
+            } else {
+                $resultado[$cor][$tamanho] = $resultado[$cor][$tamanho] + 1;
+            }
+        
+            $resultado[$cor][$tamanho] = $resultado[$cor][$tamanho];
+        
+            echo '<pre>';
+            print_r($resultado);
+            echo '</pre>';
+        }
+        
+        return $resultado;
+    }
+}
